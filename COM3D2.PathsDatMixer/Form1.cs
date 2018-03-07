b788f59892d3f9017d663f7104029b7c896ded38
scribecc <scribe.is.here@gmail.com>
2018-03-07 17:13:58 +0900

    init

---
+++ COM3D2.PathsDatMixer/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COM3D2.PathsDatMixer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string CanonicalPath(string path)
+        {
+            string cpath = Path.GetFullPath(path);
+            if (cpath.EndsWith("\\"))
+            {
+                cpath = cpath.Remove(cpath.Length - 1);
+            }
+            return cpath;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey keyCOM3D2 = Registry.CurrentUser.OpenSubKey("Software\\KISS\\カスタムオーダーメイド3D2");
+            RegistryKey keyCM3D2 = Registry.CurrentUser.OpenSubKey("Software\\KISS\\カスタムメイド3D2");
+            String pathCOM3D2 = (String)keyCOM3D2?.GetValue("InstallPath");
+            String pathCM3D2 = (String)keyCM3D2?.GetValue("InstallPath");
+
+            if (pathCOM3D2 == null)
+            {
+                MessageBox.Show("カスタムオーダーメイド3D2のインストールが確認できません。");
+                return;
+            }
+            if (pathCM3D2 == null)
+            {
+                MessageBox.Show("カスタムメイド3D2のインストールが確認できません。");
+                return;
+            }
+
+            textBox1.Text = CanonicalPath(pathCOM3D2);
+            textBox2.Text = CanonicalPath(pathCM3D2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string pathCOM3D2 = textBox1.Text;
+            string pathCM3D2 = textBox2.Text;
+
+            try
+            {
+                Program.Result result = Program.Startup(pathCOM3D2, pathCM3D2);
+                if (result != null)
+                {
+                    MessageBox.Show("マージ処理を完了しました。バックアップ: " + result.pathBackup);
+                }
+
+            } catch (IOException ex)
+            {
+                MessageBox.Show("paths.datファイルの検索に失敗しました。ディレクトリの指定先を確認してください。");
+            }
+        }
+    }
+}
---
+++ COM3D2.PathsDatMixer/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COM3D2.PathsDatMixer
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static Result Startup(string pathCOM3D2, string pathCM3D2)
+        {
+            Result result;
+
+            String fullPathCOM3D2 = Path.Combine(pathCOM3D2, "GameData\\paths.dat");
+            while (true)
+            {
+                String fullPathCOM3D2Backup = fullPathCOM3D2 + "." + DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                if (!File.Exists(fullPathCOM3D2Backup))
+                {
+                    File.Copy(fullPathCOM3D2, fullPathCOM3D2Backup);
+                    result = new Result(fullPathCOM3D2Backup);
+                    break;
+                }
+                Thread.Sleep(1000);             
+            }
+
+            PathsDatFile fileCOM3D2 = new PathsDatFile();
+            fileCOM3D2.Load(fullPathCOM3D2);
+
+            PathsDatFile fileCM3D2 = new PathsDatFile();
+            fileCM3D2.Load(Path.Combine(pathCM3D2, "GameData\\paths.dat"));
+
+            fileCOM3D2.Merge(fileCM3D2);
+            fileCOM3D2.Save(fullPathCOM3D2);
+
+            return result;
+        }
+
+        public class Result
+        {
+            private string _pathBackup;
+
+            public string pathBackup { get { return _pathBackup; } }
+
+            public Result(string backup)
+            {
+                this._pathBackup = backup;
+            }
+        }
+
+        class PathsDatFile
+        {
+            Int32 code;
+            String[] paths;
+
+            public void Load(String fname)
+            {
+                FileStream fs = new FileStream(fname, FileMode.Open);
+                BinaryReader binr = new BinaryReader(fs);
+                binr.BaseStream.Seek(12, SeekOrigin.Begin);
+
+                this.code = binr.ReadInt32();
+                Int32 count = binr.ReadInt32();
+
+                String[] paths = new String[count];
+                for (int i = 0; i < count; i++)
+                {
+                    byte b = binr.ReadByte();
+                    char[] s = binr.ReadChars(b);
+
+                    paths[i] = new String(s);
+                }
+                binr.Close();
+                fs.Close();
+                this.paths = paths;
+            }
+
+            public void Save(String fname)
+            {
+                FileStream fs = new FileStream(fname, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
+                BinaryWriter binw = new BinaryWriter(fs);
+
+                String str = "CM3D2_PATHS";
+                binw.Write((byte)str.Length);
+                binw.Write((char[])str.ToCharArray());
+
+                binw.Write(this.code);
+                binw.Write((Int32)paths.Length);
+                for (int i = 0; i < paths.Length; i++)
+                {
+                    binw.Write((byte)paths[i].Length);
+                    binw.Write((char[])paths[i].ToCharArray());
+                }
+                binw.Close();
+                fs.Close();
+            }
+
+            public void Merge(PathsDatFile file)
+            {
+                String[] paths0 = this.paths;
+                String[] paths1 = file.paths;
+
+                IEnumerable<String> newpaths = paths0.Union(paths1);
+                paths0 = newpaths.ToArray<String>();
+                Array.Sort(paths0);
+
+                this.paths = paths0;
+            }
+        }
+    }
+}
